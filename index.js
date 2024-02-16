c4ff5fbf411582bac5c3255aa8a246fee364ff7b
Raowl2002 <rahultartemh@gmail.com>
2024-02-17 00:08:20 +0530

    fixed the error of not displaying the error message

--- index.js
+++ index.js
@@ -2,24 +2,34 @@ const addTask = document.querySelector(".add input");
 const addBtn = document.querySelector(".add button");
 
 addBtn.addEventListener("click",() =>{
-    console.log(addTask.value);
     document.querySelector(".task").style.display = "block";
-    // adding the elements
-    const div = document.createElement("div");
-    div.classList.add("tasks");
-    const list = document.createElement("li");
-    const btn = document.createElement("button");
-    //Have to add the animation class so that the element disappears
-    btn.addEventListener("click",() =>{
-        div.remove();
-        
-    });
-    // added content to the added div and list and btn
-    list.textContent=addTask.value;
-    btn.innerHTML =' <i class="fa-solid fa-minus">';
-    div.appendChild(list);
-    div.appendChild(btn);
-    // adding the elements to the dom
-    document.querySelector(".task").appendChild(div);
-    addTask.value = "";
+    if(addTask.value ==""){
+        const p = document.createElement("p");
+        p.textContent = "Please enter a task!";
+        try{p.classList.add("tasks");}
+        catch(err){
+            console.log(err);
+        }
+        document.getElementById("task").appendChild(p); 
+    }else{
+        // adding the elements
+        const div = document.createElement("div");
+        div.classList.add("tasks");
+        const list = document.createElement("li");
+        const btn = document.createElement("button");
+        //Have to add the animation class so that the element disappears
+        btn.addEventListener("click",() =>{
+            div.remove();
+            
+        });
+        // added content to the added div and list and btn
+        list.textContent=addTask.value;
+        btn.innerHTML =' <i class="fa-solid fa-minus">';
+        div.appendChild(list);
+        div.appendChild(btn);
+        // adding the elements to the dom
+        document.querySelector(".task").appendChild(div);
+        addTask.value = "";
+    }
+    
 });
